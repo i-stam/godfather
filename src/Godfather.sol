55baf68a9937024f307a807d505e1cd68ad0c918
i-stam <i.stamelakos@gmail.com>
2023-04-08 13:11:40 +0300

    Add withdraw()

--- src/Godfather.sol
+++ src/Godfather.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.19;
 
 contract Godfather {
 
+    error CannotWithdraw(address);
+
     event Received(address, uint256);
+    event Withdrawal(address, uint256);
 
     address immutable godfather;
 
     uint256 unlockDate;
-    address godchild;
+    address payable godchild;
 
     constructor(address _godfather, uint256 _unlock) {
         godfather = _godfather;
@@ -17,6 +20,15 @@ contract Godfather {
 
     receive() external payable {
         emit Received(msg.sender, msg.value);
+    }
+
+    function withdraw() external {
+        if (msg.sender != godchild) {
+            revert CannotWithdraw(msg.sender);
+        }
+        uint256 contractBalance = address(this).balance;
+        godchild.transfer(contractBalance);
+        emit Withdrawal(msg.sender, contractBalance);
     }
 
 }
